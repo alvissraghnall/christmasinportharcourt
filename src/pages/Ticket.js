40e9e22d3db64899d2018ef0cf6cfd558585a7ec
codystack <ebuzzadvert@gmail.com>
2022-12-08 08:17:49 +0100

    checkout

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PaystackButton } from "react-paystack"
 import NavbarLight from '../components/NavbarLight'
 
 export default function Ticket() {
+    const publicKey = "pk_test_e25a7594d44ca2c2f1b0bb7483c44ae920b72309"
+    const [amount, setAmount] = useState("")
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState("")
+
+    const componentProps = {
+        email,
+        amount: parseInt(amount) * 100,
+        metadata: {
+            name,
+            phone,
+            email,
+            amount,
+        },
+        publicKey,
+        text: "Buy Ticket",
+        onSuccess: () => {
+            setAmount("")
+            setEmail("")
+            setName("")
+            setPhone("")
+            alert("Thanks for doing business with us! Come back soon!!")
+        },
+        onClose: () => alert("Wait! Don't leave :("),
+    }
+
   return (
     <div>
         <NavbarLight />
@@ -12,40 +40,42 @@ export default function Ticket() {
                 <div className="w-100 align-self-end pt-1 pt-md-4 pb-4" style={{maxWidth: '526px'}}>
                     <h1 className="text-center text-xl-start">Buy your Ticket</h1>
                     <form className="needs-validation" noValidate>
-                    <div className="row">
-                        <div className="col-12">
-                        <div className="position-relative mb-4">
-                            <label htmlFor="name" className="form-label fs-base">Full name</label>
-                            <input type="text" id="name" className="form-control form-control-lg" required />
-                            <div className="invalid-feedback position-absolute start-0 top-100">Please enter your name!</div>
-                        </div>
-                        </div>
-                        <div className="col-12">
-                        <div className="position-relative mb-4">
-                            <label htmlFor="email" className="form-label fs-base">Email</label>
-                            <input type="email" id="email" className="form-control form-control-lg" required />
-                            <div className="invalid-feedback position-absolute start-0 top-100">Please enter a valid email address!</div>
-                        </div>
-                        </div>
-                        <div className="col-12 mb-4">
-                            <label htmlFor="password" className="form-label fs-base">Phone </label>
-                            <div className="password-toggle">
-                                <input type="password" id="password" className="form-control form-control-lg" required />
-                                <div className="invalid-feedback position-absolute start-0 top-100">Please enter a password!</div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="position-relative mb-4">
+                                    <label htmlFor="name" className="form-label fs-base">Full name</label>
+                                    <input type="text" id="name" onChange={(e) => setName(e.target.value)} className="form-control form-control-lg" required />
+                                    <div className="invalid-feedback position-absolute start-0 top-100">Please enter your name!</div>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="position-relative mb-4">
+                                    <label htmlFor="email" className="form-label fs-base">Email</label>
+                                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} className="form-control form-control-lg" required />
+                                    <div className="invalid-feedback position-absolute start-0 top-100">Please enter a valid email address!</div>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="position-relative mb-4">
+                                    <label htmlFor="phone" className="form-label fs-base">Phone </label>
+                                    <input type="tel" id="phone" onChange={(e) => setPhone(e.target.value)} className="form-control form-control-lg" required />
+                                    <div className="invalid-feedback position-absolute start-0 top-100">Please enter a password!</div>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="position-relative mb-4">
+                                    <label htmlFor="amount" className="form-label fs-base">Select ticket type</label>
+                                    <select id="amount" onChange={(e) => setAmount(e.target.value)} className="form-select form-select-lg" required>
+                                        <option value='0' selected disabled>Choose ticket category</option>
+                                        <option value="1500">Regular</option>
+                                        <option value="5000">VIP</option>
+                                    </select>
+                                    <div className="invalid-feedback position-absolute start-0 top-100">Please choose your state!</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-12 mb-4">
-                            <label htmlFor="state" className="form-label fs-base">Select ticket type</label>
-                            <select id="state" className="form-select form-select-lg" required>
-                                <option value='0' selected disabled>Choose ticket category</option>
-                                <option value="1500">Regular</option>
-                                <option value="5000">VIP</option>
-                            </select>
-                            <div className="invalid-feedback position-absolute start-0 top-100">Please choose your state!</div>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-danger btn-lg w-100 mt-2">Buy Ticket</button>
                     </form>
+                    <PaystackButton {...componentProps} className="btn btn-danger btn-lg w-100 mt-2" />
                 </div>
                 <div className="w-100 align-self-end">
                     <span className="opacity-80">© {(new Date().getFullYear())} CIPH. All rights reserved. Built by <a href="https://www.thankgodokoro.xyz/" target="_blank" rel="noopener noreferrer">TeeGee</a></span>
